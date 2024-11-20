79d9444d596a687da11a41b81bcc35b1391029d7
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2024-11-20 22:19:26 +0530

    Delete src/components/StatusToggle.js

--- src/components/StatusToggle.js
+++
@@ -1,8 +0,0 @@
-/*
-const toggleStatus = async (id, currentStatus) => {
-    const newStatus = currentStatus === "Active" ? "Inactive" : "Active";
-    await updateCameraStatus(id, newStatus);
-    // Refresh data after updating
-    loadData();
-  };
-  */
